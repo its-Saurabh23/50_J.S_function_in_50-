0726784580f6c48f079a62538db6edc508724298
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-07-02 09:33:02 +0530

    Day_6

---
+++ Day_6.js
@@ -0,0 +1,22 @@
+var arr = [1, 3, 4, 5];
+var arr2 = [0, 9, 8, 7, 6];
+
+var newArr = []; 
+
+newArr = arr2.concat(arr);
+
+console.log(newArr);
+
+
+
+// element is added in last 
+arr2.push(22);
+
+console.log(arr2);
+
+
+
+
+arr2.unshift(33);
+
+console.log(arr2);
