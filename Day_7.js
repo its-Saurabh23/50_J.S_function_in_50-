e788a7f9dd37415871a7dde5d7bc9fda9b0089fb
Mr.Saurabh <93586145+its-Saurabh23@users.noreply.github.com>
2024-07-03 14:31:27 +0530

    day_7

---
+++ Day_7.js
@@ -0,0 +1,14 @@
+const arr = [10,20,30,40,50];
+
+const fountElemtn = arr.find((it=> it == 10));
+
+console.log(fountElemtn);
+
+
+const str = "My name is Sam, I am from India";
+ 
+console.log(str.length);
+
+const newString = str.substring(0,5);
+
+console.log(newString);
